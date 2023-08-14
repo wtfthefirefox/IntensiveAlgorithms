5db2935e34400f90b9f802f0abe20b058aa0aab2
thefirefox <wtfthefirefox@gmail.com>
2023-08-14 18:21:53 +0300

    added buble sort sol

--- BubbleSort/bubble_sort.hpp
+++ BubbleSort/bubble_sort.hpp
@@ -5,5 +5,13 @@
 // https://leetcode.com/problems/sort-an-array/
 class Solution {
 public:
-  std::vector<int> sortArray(std::vector<int> &nums) {}
+    or(size_t i = 0; i < nums.size() - 1; ++i) {
+        for(size_t g = i + 1; g < nums.size(); ++g) {
+            if (nums[i] > nums[g]) {
+                std::swap(nums[i], nums[g]);
+            }
+        }
+    }
+
+    return nums;
 };
