9c710bd3f3205ed1b8b2b2010ca684d75e30a067
Vage Ogannisian <vahe2907@gmail.com>
2023-08-14 02:56:30 +0000

    SpiralMatrix

---
+++ SpiralMatrix/spiral_matrix.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def spiral_matrix(n: int) -> List[List[int]]:
+    """
+    Returns an n x n table filled with numbers from 1 to n^2 in a spiral
+
+    Args:
+        n (int): number
+
+    Returns:
+        List[List[int]]: table filled with numbers
+    """
+
+    return []
---
+++ SpiralMatrix/test_spiral_matrix.py
@@ -0,0 +1,7 @@
+from spiral_matrix import spiral_matrix
+
+
+def test_spiral_matrix():
+    assert spiral_matrix(1) == [1]
+    assert spiral_matrix(2) == [[1, 2], [3, 4]]
+    assert spiral_matrix(3) == [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
