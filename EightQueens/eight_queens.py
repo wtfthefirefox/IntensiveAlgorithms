b9016d82aa06beb25429e93f2fd599c72bcdea01
Vage Ogannisian <vahe2907@gmail.com>
2023-08-14 03:18:40 +0000

    EightQueens

--- EightQueens/eight_queens.py
+++ EightQueens/eight_queens.py
@@ -3,10 +3,10 @@ from typing import List
 
 def eight_queens() -> List[List[int]]:
     """
-    Return a board (8 x 8) with 8 queens, where no queen attack each other
+    Returns a board (8 x 8) with 8 queens, where no queen attack each other
 
     Returns:
-        List[List[int]]: board (8 x 8) with 0 and 1 in places of queens
+        List[List[int]]: board with 0 and 1 in places of queens
     """
 
     return []
