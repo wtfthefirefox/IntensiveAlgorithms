5ada4e7e4e60234d795c306a9f1c0b492279e4d2
Vage Ogannisian <vahe2907@gmail.com>
2023-08-14 00:59:18 +0000

    add SelectionSort

---
+++ InsertionSort/insertion_sort.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def insertion_sort(array: List[int]) -> List[int]:
+    """
+    Sorts the array in ascending order using insertion sort
+
+    Args:
+        array (List[int]): input
+
+    Returns:
+        List[int]: sorted array
+    """
+
+    return []
--- InsertionSort/test_insertion_sort.py
+++ InsertionSort/test_insertion_sort.py
@@ -1,11 +1,11 @@
 from random import randint
 
-from selection_sort import selection_sort
+from insertion_sort import insertion_sort
 
 
-def test_selection_sort():
+def test_insertion_sort():
     array = []
     for i in range(5):
         array = [randint(0, 100) for j in range(10)]
 
-    assert selection_sort(array) == sorted(array)
+    assert insertion_sort(array) == sorted(array)
