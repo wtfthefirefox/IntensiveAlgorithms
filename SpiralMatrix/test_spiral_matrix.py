df39c135eb858cfb0c3ed67a20775033e3dac6c4
Vage Ogannisian <vahe2907@gmail.com>
2023-08-14 02:58:23 +0000

    SpiralMatrix

--- SpiralMatrix/test_spiral_matrix.py
+++ SpiralMatrix/test_spiral_matrix.py
@@ -2,6 +2,6 @@ from spiral_matrix import spiral_matrix
 
 
 def test_spiral_matrix():
-    assert spiral_matrix(1) == [1]
-    assert spiral_matrix(2) == [[1, 2], [3, 4]]
+    assert spiral_matrix(1) == [[1]]
+    assert spiral_matrix(2) == [[1, 2], [4, 3]]
     assert spiral_matrix(3) == [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
