e2c6d19ae1b5c4f02e82c5e8f34326ff321dd661
thefirefox <wtfthefirefox@gmail.com>
2023-08-14 19:18:57 +0300

    added insert sort

--- InsertionSort/insertion_sort.hpp
+++ InsertionSort/insertion_sort.hpp
@@ -13,5 +13,47 @@ struct ListNode {
 
 class Solution {
 public:
-    ListNode* insertionSortList(ListNode* head) {}
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode* head_copy = head;
+        std::vector<int> arr;
+
+        while(head) {
+            arr.push_back(head->val);
+
+            head = head->next;
+        }
+
+
+        for(size_t i = 1; i < arr.size(); ++i) {
+            while (i < arr.size() && arr[i] >= arr[i - 1]) {
+                ++i;
+            }
+
+            if (i == arr.size()) {
+                break;
+            }
+
+            size_t stop_pos = 0;
+
+            for(int g = i; g > 0; --g) {
+                if (arr[g] > arr[g - 1]) {
+                    break;
+                } else {
+                    std::swap(arr[g], arr[g - 1]);
+                }
+            }
+        }
+
+        head = head_copy;
+        size_t i = 0;
+
+        while(head) {
+            head->val = arr[i];
+            ++i;
+
+            head = head->next;
+        }
+
+        return head_copy;
+    }
 };
